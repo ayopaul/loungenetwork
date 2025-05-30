235b02dbfabfee06fc630da3cbd2ecc7643efdab
ayopaul <ayoblurg@gmail.com>
2025-05-30 17:57:24 +0100

    schedule fixes 2

--- app/api/auth/error/route.ts
+++
@@ -1,6 +0,0 @@
-// app/api/auth/error/route.ts
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    return NextResponse.json({ error: "Authentication failed" }, { status: 400 });
-  }
